1ba71a9670347b4d46778b8029605fde5447ec06
Saswata Som <thesaltysleuth@gmail.com>
2020-10-05 01:02:53 +0530

    Basic scripting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    public void Update()
+
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.A))
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
+            anim.SetBool("running", true);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            anim.SetBool("running", true);
+        }
+
+        else
+        {
+            anim.SetBool("running", false);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
