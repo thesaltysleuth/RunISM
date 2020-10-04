ea3968b71f267f618ef2582268ceeb7e30a159d1
Saswata Som <thesaltysleuth@gmail.com>
2020-10-04 21:12:17 +0530

    Scripting start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Transform player;
 
     public void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
-            player.localScale = new Vector2(-1, 1);
+            transform.localScale = new Vector2(-1, 1);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
-            player.localScale = new Vector2(1, 1);
+            transform.localScale = new Vector2(1, 1);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
